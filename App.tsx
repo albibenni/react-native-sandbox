c002b26ef2a71b72a9d60d1be04ce4affbfb5c36
Alberto Benatti <albi.benni8@gmail.com>
2023-06-07 07:44:50 +0200

    wip flexbox

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <TextInput placeholder="Your course goal" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal" />
         <Button title="Add goal" />
       </View>
       <View>
@@ -20,11 +20,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
   },
-  text: {
+  textInput: {
     fontSize: 18,
     margin: 16,
     padding: 16,
     borderWidth: 1,
-    backgroundColor: 'red',
+    backgroundColor: '#cccc',
+    width: '80%',
+    marginRight: 8,
+    paddingLeft: 8,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
 });
