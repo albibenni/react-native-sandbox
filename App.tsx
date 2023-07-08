91dd9575669e152cc6db244b3bdcb6ed980756a6
Alberto Benatti <albi.benni8@gmail.com>
2023-07-08 15:44:15 +0200

    adding goals

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+export default function App(): JSX.Element {
+  const [goal, setGoal] = useState<string>('');
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+  const handleAddGoal = () => {
+    setCourseGoals(prev => [...prev, goal]);
+    setGoal('');
+  };
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal" />
-        <Button title="Add goal" />
+        <TextInput
+          value={goal}
+          style={styles.textInput}
+          placeholder="Your course goal"
+          onChange={e => setGoal(e.nativeEvent.text)}
+        />
+        <Button title="Add goal" onPress={handleAddGoal} />
       </View>
       <View style={styles.goals}>
         <Text>List of Goals..</Text>
       </View>
+      {courseGoals.map(g => (
+        <Text>{g}</Text>
+      ))}
     </View>
   );
 }
