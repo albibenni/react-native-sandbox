bd8a4dfc06dfeaff186ef4a0dbe38f8549da4469
Alberto Benatti <albi.benni8@gmail.com>
2023-07-09 10:38:07 +0200

    trying detecting pointers

--- App.tsx
+++ App.tsx
@@ -1,17 +1,59 @@
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  NativePointerEvent,
+  NativeSyntheticEvent,
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+} from 'react-native';
+import { Path, Svg } from 'react-native-svg';
+const { height, width } = Dimensions.get('window');
 
 export default function App(): JSX.Element {
   const [goal, setGoal] = useState<string>('');
   const [courseGoals, setCourseGoals] = useState<string[]>([]);
+  const [path, setPath] = useState<string[]>([]);
 
-  const handleAddGoal = () => {
-    setCourseGoals(prev => [...prev, goal]);
-    setGoal('');
+  const onMove = (event: NativeSyntheticEvent<NativePointerEvent>): void => {
+    const {
+      nativeEvent: { offsetX, offsetY },
+    } = event;
+    const newPath = [...path];
+
+    // Create a new point
+    const newPoint = `${newPath.length === 0 ? 'M' : ''}${offsetX.toFixed(
+      0
+    )},${offsetY.toFixed(0)} `;
+
+    // Add new point to existing points
+    newPath.push(newPoint);
+    setPath(newPath);
   };
+  // const handleAddGoal = () => {
+  //   setCourseGoals(prev => [...prev, goal]);
+  //   setGoal('');
+  // };
+  // const handleAddGoal = (event: GestureResponderEvent) => {
+  //   const isPencilTouch = !!event.nativeEvent.altitudeAngle;
+  // };
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
+      <Text style={styles.label}>Draw Something</Text>
+      <View style={styles.wrapper} onPointerMove={onMove}>
+        <Svg height={height * 0.8} width={width}>
+          <Path
+            d={path.join('')}
+            stroke={'blue'}
+            fill={'transparent'}
+            strokeWidth={2}
+            strokeLinejoin={'round'}
+            strokeLinecap={'round'}
+          />
+        </Svg>
+      </View>
+
+      {/* <View style={styles.inputContainer}>
         <TextInput
           value={goal}
           style={styles.textInput}
@@ -27,12 +69,23 @@ export default function App(): JSX.Element {
             <Text>{g}</Text>
           </View>
         ))}
-      </View>
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    borderColor: 'blue',
+    borderWidth: 2,
+    height: height * 0.8,
+    width,
+  },
+  label: {
+    color: 'blue',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
   container: {
     flex: 1,
     paddingTop: 50,
